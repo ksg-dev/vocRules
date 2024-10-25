d6720b38be5ed45aea14a755b8b810801bf3d12c
ksg-dev <ksg.dev.data@gmail.com>
2024-10-25 13:53:33 -0500

    read rule pdf and write to text file

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-import requests
-from bs4 import BeautifulSoup
+from PyPDF2 import PdfReader
+from pprint import pprint
 
-SCAQMD_1113 = "https://www.aqmd.gov/docs/default-source/rule-book/reg-xi/r1113.pdf?sfvrsn=28"
+reader = PdfReader("rules/r1113.pdf")
+pages = reader.pages
+outline = reader.outline
+
+# for page in pages:
+#     print(page.extract_text())
+
+
+
+# def get_def_destination():
+#     for head in outline:
+#         if "Definitions" in head["/Title"]:
+#             dest_data = {
+#                 "page": head["/Page"],
+#                 "title": head["/Title"]
+#             }
+#             return dest_data
+
+
+def get_text():
+    with open("1113.txt", "w") as file:
+        for page in pages:
+            file.write(page.extract_text())
+
+    print("All Done!")
+
+get_text()
+
